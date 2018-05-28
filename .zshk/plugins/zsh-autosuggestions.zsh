b29f5bf57a638128cdd74b0b2a43af679df29147
shkschneider <shk.schneider@gmail.com>
2018-05-28 15:37:46 +0200

    zshk plugins: sets ZSH_CUSTOM to ZSHK as fallback

--- .zshk/plugins/zsh-autosuggestions.zsh
+++ .zshk/plugins/zsh-autosuggestions.zsh
@@ -1,6 +1,8 @@
 # @author shkschneider
 # zsh-autosuggestions.zsh
 
+ZSH_CUSTOM=${ZSH_CUSTOM:=$ZSHK}
+
 if [ -f "$ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ] ; then
     source "$ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=white,underline"
--- .zshk/plugins/zsh-syntax-highlighting.zsh
+++ .zshk/plugins/zsh-syntax-highlighting.zsh
@@ -1,6 +1,8 @@
 # @author shkschneider
 # zsh-syntax-highlight.zsh
 
+ZSH_CUSTOM=${ZSH_CUSTOM:=$ZSHK}
+
 if [ -f "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ] ; then
     zle_highlight=()
     source "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
