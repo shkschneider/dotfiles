e01809e6f94b8c9906f9b3cc4482a8a626394c55
shkschneider <shk.schneider@gmail.com>
2018-06-21 16:50:12 +0200

    plugins: git-prompt, improved yet again

--- .zshk/plugins/git-prompt/git-prompt.plugin.zsh
+++ .zshk/plugins/git-prompt/git-prompt.plugin.zsh
@@ -1,5 +1,5 @@
 # @author shkschneider
-# git-prompt.plugin.zsh (oh-my-zsh compatible)
+# git-prompt.plugin.zsh
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
@@ -15,15 +15,18 @@ function git_prompt_info() {
     echo -n $ZSH_THEME_GIT_PROMPT_PREFIX
     git_dir=$(git rev-parse --git-dir 2>/dev/null)
     if [ -n "$git_dir" ] && [ $git_dir != "." ] ; then # exclude bare
+        git_repo=$(git config --get remote.origin.url 2>/dev/null | rev | cut -d'/' -f1 | rev | sed 's;\.git;;')
+        [ -n "$git_repo" ] && echo -n $git_repo$ZSH_THEME_GIT_PROMPT_SEPARATOR
         if [ -z "$(git branch -vv --no-color 2>/dev/null)" ] ; then # naked
             echo -n $ZSH_THEME_GIT_PROMPT_UNTRACKED
         elif [ -z "$(git symbolic-ref HEAD 2>/dev/null)" ] ; then # detached
-            git_branch=$(git describe --all --contains HEAD | cut -d'~' -f1)
+            git_branch=$(git describe --all --contains HEAD 2>/dev/null | cut -d'~' -f1 | cut -d'^' -f1)
             echo -n $git_branch$ZSH_THEME_GIT_PROMPT_DIRTY
             # [ -d "$git_dir/rebase-apply" ] || [ -d "$git_dir/rebase-merge" ] # rebasing
         else
             git_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
             git_remote=$(git rev-parse --abbrev-ref @{u} 2>/dev/null)
+            git_remote=${git_remote/@{u}/}
             if [ -n "$git_remote" ] ; then
                 echo -n $git_remote$ZSH_THEME_GIT_PROMPT_REMOTE$git_branch
                 git_ahead=$(git rev-list $git_remote..HEAD 2>/dev/null | wc -l)
