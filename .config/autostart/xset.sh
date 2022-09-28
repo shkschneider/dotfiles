58db0373bcf6df0d2b7575d22bf24c625da9e842
shkschneider <shk.schneider@gmail.com>
2022-09-28 11:25:07 +0200

    ci

--- .config/autostart/xset.sh
+++ .config/autostart/xset.sh
@@ -2,5 +2,7 @@
 
 # disable Display Power Management Signaling and prevent screen from blanking
 xset s off -dpms
+# should preserve setting even after force off for example
+xset dpms 0 0 0
 
 # EOF
